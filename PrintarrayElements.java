b7a32ff20232386fc7d4d24437d5956c7f5ae663
Prathvi06172005 <prathvi.23cs103@sode-edu.in>
2024-10-17 11:09:59 +0530

    Add files via upload

---
+++ PrintarrayElements.java
@@ -0,0 +1,10 @@
+public class PrintarrayElements{
+public static void main(String[]args){
+int[] numbers={10,20,30,45,50};
+System.out.println("Array elements:");
+for (int i=0;i<numbers.length;i++){
+System.out.println("enter at index" + i + ": "+ numbers[i]);
+}
+}
+}
+ 
