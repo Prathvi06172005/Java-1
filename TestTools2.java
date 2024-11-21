6a3b8b85750aa80de27ccee0417337cc783ab50e
Prathvi06172005 <prathvi.23cs103@sode-edu.in>
2024-11-21 09:28:57 +0530

    Add files via upload

---
+++ TestTools2.java
@@ -0,0 +1,13 @@
+package pkg2;
+import pkg1.Tools;
+public class TestTools2 {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        t1.number(10);
+        t1.even(10);
+        t1.odd(10);
+        t1.multipleof3(10);
+        t1.multipleof4(10);
+        t1.multipleof5(10);
+    }    
+}
